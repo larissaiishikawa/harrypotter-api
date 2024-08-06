7be476ec6cc00b6f699e7a6b433eec71052a2891
Larissa Ishikawa <l.ishikawacunha@gmail.com>
2024-08-06 20:29:33 -0300

    fix: Fixed index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express()
 const port = 3000
-mongoose.connect('mongodb+srv://larissaishikawa:<password>@harrypotter-api.6obyz.mongodb.net/?retryWrites=true&w=majority&appName=harrypotter-api');
+mongoose.connect('mongodb+srv://larissaishikawa:SUcjlIZ6Z1Q5ZRIY@harrypotter-api.6obyz.mongodb.net/?retryWrites=true&w=majority&appName=harrypotter-api');
 
 const Movie = mongoose.model('Movie', {
     title: String,
@@ -14,6 +14,18 @@ const Movie = mongoose.model('Movie', {
 
 app.get("", (req, res) => {
     res.send("Hello World!")
+})
+
+app.post("/", async (req, res) => {
+    const movie = new Movie({
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer_url: req.body.trailer_url
+    })
+
+    await movie.save()
+    res.send(movie)
 })
 
 app.listen(port, () => {
